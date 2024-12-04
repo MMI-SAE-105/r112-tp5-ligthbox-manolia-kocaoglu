320b9395b8640161b430859f05d08a81e069ecd4
KOCAOGLU Manolia <157006573+manolia-kocaoglu@users.noreply.github.com>
2024-12-04 14:42:15 +0100

    continuité du js

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,13 @@ lb.addEventListener("click", function() {
     lb.close();
     });
 
+    const images = document.querySelectorAll("[data-full-img]"); 
+
+    images.forEach(image => {
+        image.addEventListener("click", function() {
+            const fullImg = image.dataset.fullImg;
+            const imgElement = lb.querySelector("img");
+            imgElement.src = fullImg;
+            lb.showModal();
+        });
+    });
